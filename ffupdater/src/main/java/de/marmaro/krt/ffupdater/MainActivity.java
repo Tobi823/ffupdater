4176c9d80a951a2ef3f30e8c6ec22a7a6a94c3f1
Boris Kraut <krt@nurfuerspam.de>
2015-08-10 16:33:09 +0200

    Use actionbar

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/MainActivity.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/MainActivity.java
@@ -24,9 +24,11 @@ import android.widget.Toast;
 
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -35,7 +37,7 @@ import android.view.ViewGroup;
 import android.preference.PreferenceManager;
 import android.content.SharedPreferences;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
 	private static final String TAG = "MainActivity";
 	private static final String CV = "39.0";
@@ -208,11 +210,27 @@ public class MainActivity extends Activity {
 			}
 		});
 
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-		Intent preftest = new Intent(this, MyPreferencesActivity.class);
-		startActivity(preftest);
-
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+			case R.id.action_prefs:
+				Intent preftest = new Intent(this, MyPreferencesActivity.class);
+				startActivity(preftest);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/MyPreferencesActivity.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/MyPreferencesActivity.java
@@ -3,24 +3,25 @@ package de.marmaro.krt.ffupdater;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.content.res.Configuration;
 
 public class MyPreferencesActivity extends PreferenceActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+	}
 
-    public static class MyPreferenceFragment extends PreferenceFragment
-    {
-        @Override
-        public void onCreate(final Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-        }
-    }
-
+	public static class MyPreferenceFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(final Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preferences);
+		}
+	}
 }
 
