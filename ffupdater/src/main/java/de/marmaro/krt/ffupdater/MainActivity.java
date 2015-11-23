dfedc0f6e004521dee2c628f8a503f58c6e65c4b
Boris Kraut <krt@nurfuerspam.de>
2015-11-23 16:13:51 +0100

    Bump to 42.0.1 (8)

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/MainActivity.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/MainActivity.java
@@ -40,7 +40,7 @@ import android.content.SharedPreferences;
 public class MainActivity extends ActionBarActivity {
 
 	private static final String TAG = "MainActivity";
-	private static final String CV = "42.0";
+	private static final String CV = "42.0.1";
 
 	public void downloadAndInstall(String uri, final int vc) {
 
