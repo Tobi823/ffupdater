0c136de90ddde443ff8125905efee01b9b4468de
Tobiwan <Tobi823@users.noreply.github.com>
2018-07-14 18:13:01 +0200

    Refactor download code
    - increase minSdkVersion to 19 (Android 4.4) for being able to use try-with-resources
    (closes connections more reliably; even IOUtils.closeQuietly is deprecated
    since 2.6 because try-with-resources is better)
    - add library commons-io:commons-io:2.6 for a better download of the text
    from https://product-details.mozilla.org/1.0/mobile_versions.json

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/MozillaVersions.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/MozillaVersions.java
@@ -2,13 +2,11 @@ package de.marmaro.krt.ffupdater;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
-import javax.net.ssl.HttpsURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,29 +14,17 @@ import android.util.Log;
 
 public class MozillaVersions {
 	private static final String TAG = "ffupdater";
-	private static final String checkUri = "https://product-details.mozilla.org/1.0/mobile_versions.json";
+	private static final String CHECK_URL = "https://product-details.mozilla.org/1.0/mobile_versions.json";
 
 	private static String downloadVersion() {
-		HttpsURLConnection urlConnection = null;
-		StringBuilder result = new StringBuilder();
 		try {
-			URL url = new URL(checkUri);
-			urlConnection = (HttpsURLConnection) url.openConnection();
-			InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-			String line;
-			while ((line = reader.readLine()) != null) {
-				result.append(line);
+			URL url = new URL(CHECK_URL);
+			try (InputStream is = url.openConnection().getInputStream()) {
+				return IOUtils.toString(is, StandardCharsets.UTF_8.name());
 			}
-			return result.toString();
 		} catch (IOException e) {
 			Log.e(TAG, "Error: " + e);
 			return "";
-		} finally {
-			if (urlConnection != null) {
-            	urlConnection.disconnect();
-			}
 		}
 	}
 
