30708ea1c9bfe85e09d7744b64ea983854fe9f67
Tobiwan <Tobi823@users.noreply.github.com>
2018-07-16 19:18:47 +0200

    Javadoc
    - add javadoc to DownloadOnClick.java

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/DownloadOnClick.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/DownloadOnClick.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 import android.view.View;
 
 /**
+ * This class is a {@link android.view.View.OnClickListener} which will open the download url for
+ * a specific stage in the webbrowser (but only if the download url is available in @{@link DownloadUrl}).
  * Created by Tobiwan on 15.07.2018.
  */
 public class DownloadOnClick implements View.OnClickListener {
