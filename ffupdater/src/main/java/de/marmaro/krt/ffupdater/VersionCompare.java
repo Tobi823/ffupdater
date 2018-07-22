a0c949c1b3df32c143a0e83fe6162703c15237ae
Tobiwan <Tobi823@users.noreply.github.com>
2018-07-22 19:15:53 +0200

    Update VersionCompare.java for comparing Maps with Versions

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/VersionCompare.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/VersionCompare.java
@@ -2,9 +2,8 @@ package de.marmaro.krt.ffupdater;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * This class can compare the different version numbers from the available and local installed firefox apps.
@@ -15,56 +14,24 @@ public class VersionCompare {
 
     /**
      * Check if for an installed firefox (release, beta, nightly) an update is available.
-     * @param mobileVersions
-     * @param localVersions
+     * @param available
+     * @param installed
      * @return
      */
-    public static List<UpdateChannel> isUpdateAvailable(MobileVersions mobileVersions, LocalInstalledVersions localVersions) {
+    public static List<UpdateChannel> isUpdateAvailable(Map<UpdateChannel, Version> available, Map<UpdateChannel, Version> installed) {
         List<UpdateChannel> updates = new ArrayList<>();
 
-        for (UpdateChannel updateChannel : UpdateChannel.values()) {
-            if (isNewVersionAvailable(mobileVersions, localVersions, updateChannel)) {
-                updates.add(updateChannel);
+        for (Map.Entry<UpdateChannel, Version> installedVersion : installed.entrySet()) {
+            Version availableVersion = available.get(installedVersion.getKey());
+            if (isDifferent(availableVersion, installedVersion.getValue())) {
+                updates.add(installedVersion.getKey());
             }
         }
 
         return updates;
     }
 
-    /**
-     * Check if for a specific update channel (release, beta, nightly) an update is available.
-     * @param mobileVersions
-     * @param localVersions
-     * @param updateChannel
-     * @return
-     */
-    private static boolean isNewVersionAvailable(MobileVersions mobileVersions, LocalInstalledVersions localVersions, UpdateChannel updateChannel) {
-        if (!localVersions.isPresent(updateChannel)) {
-            return false;
-        }
-
-        String remote = filterVersionString(mobileVersions.getValueBy(updateChannel), updateChannel);
-        String local = localVersions.getVersionString(updateChannel).getName();
-        return !Objects.equals(remote, local);
-    }
-
-    /**
-     * The latest version number for firefox beta is incorrect. The rest interface returns e.g "62.0b7" but
-     * the app as the version "62.0"
-     * @param version
-     * @param updateChannel
-     * @return
-     */
-    private static String filterVersionString(String version, UpdateChannel updateChannel) {
-        if (UpdateChannel.BETA == updateChannel) {
-            Pattern pattern = Pattern.compile("\\d+\\.\\d");
-            Matcher match = pattern.matcher(version);
-            if (match.find()) {
-                return match.group();
-            } else {
-                return version;
-            }
-        }
-        return version;
+    private static boolean isDifferent(Version available, Version installed) {
+        return null != available && null != installed && !Objects.equals(available.getName(), installed.getName());
     }
 }
