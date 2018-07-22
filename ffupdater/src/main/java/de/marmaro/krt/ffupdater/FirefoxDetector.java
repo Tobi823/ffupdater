648a66681e3e19efe41952e9987465762917a723
Tobiwan <Tobi823@users.noreply.github.com>
2018-07-22 19:01:39 +0200

    Replace LocalInstalledVersions.java with Map<UpdateChannel, Version>
    - remove LocalInstalledVersions.java
    - FirefoxDetector.java returns a Map

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/FirefoxDetector.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/FirefoxDetector.java
@@ -2,7 +2,8 @@ package de.marmaro.krt.ffupdater;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.support.annotation.Nullable;
+
+import com.github.dmstocking.optional.java.util.Optional;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,10 +15,12 @@ public class FirefoxDetector {
     private static final String RELEASE_PACKAGE_ID = "org.mozilla.firefox";
     private static final String BETA_PACKAGE_ID = "org.mozilla.firefox_beta";
     private static final String NIGHTLY_PACKAGE_ID = "org.mozilla.fennec_aurora";
+    private static final String FOCUS_PACKAGE_ID = "org.mozilla.focus";
+    private static final String KLAR_PACKAGE_ID = "org.mozilla.klar";
 
-    private LocalInstalledVersions localVersions;
+    private Map<UpdateChannel, Version> localVersions;
 
-    private FirefoxDetector(LocalInstalledVersions localVersions) {
+    private FirefoxDetector(Map<UpdateChannel, Version> localVersions) {
         this.localVersions = localVersions;
     }
 
@@ -28,21 +31,23 @@ public class FirefoxDetector {
      * @return
      */
     public static FirefoxDetector create(PackageManager packageManager) {
-        LocalInstalledVersions localVersions = new LocalInstalledVersions();
+        Map<UpdateChannel, Version> installed = new HashMap<>();
 
-        Map<UpdateChannel, String> mapping = new HashMap<>();
-        mapping.put(UpdateChannel.RELEASE, RELEASE_PACKAGE_ID);
-        mapping.put(UpdateChannel.BETA, BETA_PACKAGE_ID);
-        mapping.put(UpdateChannel.NIGHTLY, NIGHTLY_PACKAGE_ID);
+        Map<UpdateChannel, String> packageIds = new HashMap<>();
+        packageIds.put(UpdateChannel.RELEASE, RELEASE_PACKAGE_ID);
+        packageIds.put(UpdateChannel.BETA, BETA_PACKAGE_ID);
+        packageIds.put(UpdateChannel.NIGHTLY, NIGHTLY_PACKAGE_ID);
+        packageIds.put(UpdateChannel.FOCUS, FOCUS_PACKAGE_ID);
+        packageIds.put(UpdateChannel.KLAR, KLAR_PACKAGE_ID);
 
-        for (Map.Entry<UpdateChannel, String> entry : mapping.entrySet()) {
-            Version version = getVersion(entry.getValue(), packageManager);
-            if (null != version) {
-                localVersions.setVersion(entry.getKey(), version);
+        for (Map.Entry<UpdateChannel, String> packageId : packageIds.entrySet()) {
+            Optional<Version> version = getVersion(packageId.getValue(), packageManager);
+            if (version.isPresent()) {
+                installed.put(packageId.getKey(), version.get());
             }
         }
 
-        return new FirefoxDetector(localVersions);
+        return new FirefoxDetector(installed);
     }
 
     /**
@@ -51,20 +56,20 @@ public class FirefoxDetector {
      * @param packageManager
      * @return
      */
-    @Nullable
-    private static Version getVersion(String packageName, PackageManager packageManager) {
+    private static Optional<Version> getVersion(String packageName, PackageManager packageManager) {
         try {
             PackageInfo releasePackage = packageManager.getPackageInfo(packageName, 0);
-            return new Version(releasePackage.versionName, releasePackage.versionCode);
+            Version version = new Version(releasePackage.versionName, releasePackage.versionCode);
+            return Optional.of(version);
         } catch (PackageManager.NameNotFoundException e) {
-            return null;
+            return Optional.empty();
         }
     }
 
     /**
      * @return Return all versions of installed firefox apps.
      */
-    public LocalInstalledVersions getLocalVersions() {
+    public Map<UpdateChannel, Version> getLocalVersions() {
         return localVersions;
     }
 }
