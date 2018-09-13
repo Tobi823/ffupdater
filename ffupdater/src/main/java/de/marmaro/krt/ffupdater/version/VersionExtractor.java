fae38ea5d313053bea63351a1fd03abba0a09bfb
Tobi823 <Tobi823@users.noreply.github.com>
2018-09-13 16:21:25 +0200

    Fix bug with focus/klar version nam - I didn't expect that they switch from two digit version string (6.1) to three digit version string (6.1.1)

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/version/VersionExtractor.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/version/VersionExtractor.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 
 import de.marmaro.krt.ffupdater.UpdateChannel;
 import de.marmaro.krt.ffupdater.api.ApiResponses;
+import de.marmaro.krt.ffupdater.api.mozilla.MobileVersions;
 
 import static de.marmaro.krt.ffupdater.UpdateChannel.BETA;
 import static de.marmaro.krt.ffupdater.UpdateChannel.FOCUS;
@@ -19,7 +20,7 @@ import static de.marmaro.krt.ffupdater.UpdateChannel.RELEASE;
  */
 public class VersionExtractor {
 
-    public static final String REGEX_EXTRACT_VERSION = "\\d+\\.\\d";
+    public static final String REGEX_EXTRACT_VERSION = "\\d+(\\.\\d)+";
 
     private ApiResponses apiResponses;
 
@@ -30,18 +31,24 @@ public class VersionExtractor {
     public Map<UpdateChannel, Version> getVersionStrings() {
         Map<UpdateChannel, Version> versionStrings = new HashMap<>();
 
-        versionStrings.put(RELEASE, new Version(apiResponses.getMozillaApiResponse().getStableVersion(), 0));
-        String rawBetaVersion = apiResponses.getMozillaApiResponse().getBetaVersion();
-        versionStrings.put(BETA, new Version(extractVersion(rawBetaVersion, REGEX_EXTRACT_VERSION),0));
-        versionStrings.put(NIGHTLY, new Version(apiResponses.getMozillaApiResponse().getNightlyVersion(), 0));
+        MobileVersions mozillaApiResponse = apiResponses.getMozillaApiResponse();
+        versionStrings.put(RELEASE, new Version(mozillaApiResponse.getStableVersion(), 0));
+        versionStrings.put(BETA, new Version(mozillaApiResponse.getBetaVersion(),0));
+        versionStrings.put(NIGHTLY, new Version(mozillaApiResponse.getNightlyVersion(), 0));
 
-        String releaseName = apiResponses.getGithubApiResponse().getName();
-        versionStrings.put(FOCUS, new Version(extractVersion(releaseName, REGEX_EXTRACT_VERSION), 0));
-        versionStrings.put(KLAR, new Version(extractVersion(releaseName, REGEX_EXTRACT_VERSION), 0));
-
+        String githubResponse = apiResponses.getGithubApiResponse().getName();
+        String focusKlar = extractVersion(githubResponse, REGEX_EXTRACT_VERSION);
+        versionStrings.put(FOCUS, new Version(focusKlar, 0));
+        versionStrings.put(KLAR, new Version(focusKlar, 0));
         return versionStrings;
     }
 
+    /**
+     * Get the version number from the github release name (for example "Focus / Klar - v6.1.1" => "6.1.1")
+     * @param raw
+     * @param regex
+     * @returnN
+     */
     private static String extractVersion(String raw, String regex) {
         Pattern pattern = Pattern.compile(regex);
         Matcher match = pattern.matcher(raw);
---
+++ ffupdater/src/test/java/de/marmaro/krt/ffupdater/version/VersionExtractorTest.java
@@ -0,0 +1,42 @@
+package de.marmaro.krt.ffupdater.version;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import de.marmaro.krt.ffupdater.UpdateChannel;
+import de.marmaro.krt.ffupdater.api.ApiResponses;
+import de.marmaro.krt.ffupdater.api.github.GithubApiConsumer;
+import de.marmaro.krt.ffupdater.api.github.Release;
+import de.marmaro.krt.ffupdater.api.mozilla.MobileVersions;
+import de.marmaro.krt.ffupdater.api.mozilla.MozillaApiConsumer;
+
+import static de.marmaro.krt.ffupdater.version.VersionExtractor.REGEX_EXTRACT_VERSION;
+import static org.junit.Assert.*;
+
+public class VersionExtractorTest {
+
+    private ApiResponses createApiResponse(){
+        MobileVersions mobileVersions = new MobileVersions();
+        mobileVersions.setStableVersion("62.0.1");
+        mobileVersions.setBetaVersion("63.0b5");
+        mobileVersions.setNightlyVersion("64.0a1");
+
+        Release release = new Release();
+        release.setName("Focus / Klar - v6.1.1");
+
+        return new ApiResponses(mobileVersions, release);
+    }
+
+    @Test
+    public void getVersionStrings() {
+        VersionExtractor extractor = new VersionExtractor(createApiResponse());
+        System.out.println(REGEX_EXTRACT_VERSION);
+        Map<UpdateChannel, Version> actual = extractor.getVersionStrings();
+        assertEquals("62.0.1", actual.get(UpdateChannel.RELEASE).getName());
+        assertEquals("63.0b5", actual.get(UpdateChannel.BETA).getName());
+        assertEquals("64.0a1", actual.get(UpdateChannel.NIGHTLY).getName());
+        assertEquals("6.1.1", actual.get(UpdateChannel.FOCUS).getName());
+        assertEquals("6.1.1", actual.get(UpdateChannel.KLAR).getName());
+    }
+}
