ef80cd65007462ee5064f307127f5e61681e6fbb
Tobiwan <tobiwanop@posteo.de>
2019-06-26 19:20:02 +0200

    Tobiwan/ffupdater#8 - Register/Update/Unregister UpdateChecker when changing values in Settings

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/settings/SettingsActivity.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/settings/SettingsActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import de.marmaro.krt.ffupdater.R;
+import de.marmaro.krt.ffupdater.background.UpdateChecker;
 
 
 public class SettingsActivity extends AppCompatActivity {
@@ -33,6 +35,16 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.settings, rootKey);
+            Preference prefCheckInterval = findPreference(getString(R.string.pref_check_interval));
+            prefCheckInterval.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    String valueAsString = (String) newValue;
+                    int value = Integer.parseInt(valueAsString);
+                    UpdateChecker.registerOrUnregister(value);
+                    return true;
+                }
+            });
         }
     }
 }
