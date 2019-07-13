9e5355551b7194a1be05bed8f13b36dbe30e943b
Tobiwan <tobiwanop@posteo.de>
2019-07-13 18:13:31 +0200

    Tobiwan/ffupdater#13 - use android.os.Build.SUPPORTED_ABIS and android.os.Build.CPU_ABI for determining the ABI of the device. Instead of System.getProperty("os.arch"), SUPPORTED_ABIS and CPU_ABI works more reliably

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/DownloadUrl.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/DownloadUrl.java
@@ -1,48 +1,43 @@
 package de.marmaro.krt.ffupdater;
 
-import com.google.common.base.Preconditions;
+import java.util.Arrays;
 
-import java.security.InvalidParameterException;
+import static android.os.Build.CPU_ABI;
+import static android.os.Build.SUPPORTED_ABIS;
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
 
 /**
  * This class builds (depending on the architecture) the download url.
  * Furthermore this class can validate if the api level is high enough for running firefox.
  */
 public class DownloadUrl {
-    private static final String NON_X86_ARCHITECTURE = "android";
-    private static final String X86_ARCHITECTURE = "android-x86";
+    private static final String DEFAULT_OS = "android";
+    private static final String X86_OS = "android-x86";
     private static final String RELEASE_PRODUCT = "fennec-latest";
     private static final String BETA_PRODUCT = "fennec-beta-latest";
     private static final String NIGHTLY_PRODUCT = "fennec-nightly-latest";
 
     private static final String URL_TEMPLATE = "https://download.mozilla.org/?product=%s&os=%s&lang=multi";
-
-    private static final String PROPERTY_OS_ARCHITECTURE = "os.arch";
+    private static final String X86_ARCH = "x86";
+    private static final String X64_ARCH = "x86_64";
 
     /**
-     * Return the URL for the given architecture and update channel
+     * Return the URL for the given operating system and update channel
      * Update URI as specified in https://archive.mozilla.org/pub/mobile/releases/latest/README.txt
      *
-     * @param updateChannel
-     * @return
+     * @param updateChannel which fennec browser should be used? (released, beta or nightly)
+     * @return url for downloading the fennec browser
      */
     public static String getUrl(String updateChannel) {
-        String product = getProduct(updateChannel);
-        String architecture = getMozillaApiArchitecture();
-        return String.format(URL_TEMPLATE, product, architecture);
+        return String.format(URL_TEMPLATE, getProduct(updateChannel), getOperatingSystem());
     }
 
-    private static String getMozillaApiArchitecture() {
-        String architecture = System.getProperty(PROPERTY_OS_ARCHITECTURE);
-        Preconditions.checkNotNull(architecture, "architecture must not be null");
-
-        switch (architecture) {
-            case "i686":
-            case "x86_64":
-                return X86_ARCHITECTURE;
-            default:
-                return NON_X86_ARCHITECTURE;
+    private static String getOperatingSystem() {
+        if (SDK_INT >= LOLLIPOP) {
+            return Arrays.asList(SUPPORTED_ABIS).contains(X86_ARCH) ? X86_OS : DEFAULT_OS;
         }
+        return CPU_ABI.equals(X86_ARCH) || CPU_ABI.equals(X64_ARCH) ? X86_OS : DEFAULT_OS;
     }
 
     private static String getProduct(String updateChannel) {
