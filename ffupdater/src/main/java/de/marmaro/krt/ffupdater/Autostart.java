6f6431422664f3286b8bc330ac610a72bf040bab
Tobiwan <tobiwanop@posteo.de>
2019-10-04 19:08:34 +0200

    Tobiwan/ffupdater#14 Tobi823/ffupdater#4 - rename AutoStartUpdateChecker to Autostart

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/Autostart.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/Autostart.java
@@ -1,4 +1,4 @@
-package de.marmaro.krt.ffupdater.background;
+package de.marmaro.krt.ffupdater;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -10,7 +10,7 @@ import de.marmaro.krt.ffupdater.NotificationCreator;
  * After the phone is booted, this class will start the {@link NotificationCreator}.
  * Reason: FFUpdater should be able to check for updates even after a phone reboot.
  */
-public class AutoStartUpdateChecker extends BroadcastReceiver {
+public class Autostart extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent != null && Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
