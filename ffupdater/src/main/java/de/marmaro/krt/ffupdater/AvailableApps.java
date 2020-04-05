49124a753b654adf1f4ec27a78a4a9d379d43241
Tobiwan <tobiwanop@posteo.de>
2020-04-05 14:38:10 +0200

    - make AvailableApps.java more readable - only add version/url for focus or klar if they are in appsToCheck

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/AvailableApps.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/AvailableApps.java
@@ -13,6 +13,14 @@ import de.marmaro.krt.ffupdater.download.github.FenixVersionFinder;
 import de.marmaro.krt.ffupdater.download.github.FirefoxLiteVersionFinder;
 import de.marmaro.krt.ffupdater.download.github.FocusVersionFinder;
 
+import static de.marmaro.krt.ffupdater.App.FENIX;
+import static de.marmaro.krt.ffupdater.App.FENNEC_BETA;
+import static de.marmaro.krt.ffupdater.App.FENNEC_NIGHTLY;
+import static de.marmaro.krt.ffupdater.App.FENNEC_RELEASE;
+import static de.marmaro.krt.ffupdater.App.FIREFOX_FOCUS;
+import static de.marmaro.krt.ffupdater.App.FIREFOX_KLAR;
+import static de.marmaro.krt.ffupdater.App.FIREFOX_LITE;
+
 /**
  * Retrieve the latest version names for a specific set of apps.
  * Get the download url for a specific app and compare version names to determine an available update.
@@ -31,7 +39,7 @@ public class AvailableApps {
      * Set flag to download a specific app.
      * The app that will be downloaded will be automatically added to {@code appsToCheck}.
      *
-     * @param appsToCheck apps for update checks
+     * @param appsToCheck   apps for update checks
      * @param appToDownload app for download
      * @return object containing all version names for {@code appsToCheck}.
      */
@@ -54,58 +62,91 @@ public class AvailableApps {
     public static AvailableApps create(Set<App> appsToCheck) {
         AvailableApps result = new AvailableApps();
 
-        if (appsToCheck.contains(App.FENNEC_RELEASE) ||
-                appsToCheck.contains(App.FENNEC_BETA) ||
-                appsToCheck.contains(App.FENNEC_NIGHTLY)) {
-            Optional<OfficialApi.Version> response = OfficialApi.getResponse();
-            if (response.isPresent() && appsToCheck.contains(App.FENNEC_RELEASE)) {
-                result.versions.put(App.FENNEC_RELEASE, response.get().getReleaseVersion());
-                result.downloadUrl.put(App.FENNEC_RELEASE, getDownloadUrlForFennec(App.FENNEC_RELEASE, response.get()));
-            }
-            if (response.isPresent() && appsToCheck.contains(App.FENNEC_BETA)) {
-                result.versions.put(App.FENNEC_BETA, response.get().getBetaVersion());
-                result.downloadUrl.put(App.FENNEC_BETA, getDownloadUrlForFennec(App.FENNEC_BETA, response.get()));
-            }
-            if (response.isPresent() && appsToCheck.contains(App.FENNEC_NIGHTLY)) {
-                result.versions.put(App.FENNEC_NIGHTLY, response.get().getNightlyVersion());
-                result.downloadUrl.put(App.FENNEC_NIGHTLY, getDownloadUrlForFennec(App.FENNEC_NIGHTLY, response.get()));
-            }
+        if (appsToCheck.contains(FENNEC_RELEASE) ||
+                appsToCheck.contains(FENNEC_BETA) ||
+                appsToCheck.contains(FENNEC_NIGHTLY)) {
+            checkFennec(result, appsToCheck);
         }
 
-        if (appsToCheck.contains(App.FIREFOX_KLAR) ||
-                appsToCheck.contains(App.FIREFOX_FOCUS)) {
-            FocusVersionFinder focusVersionFinder = FocusVersionFinder.create();
-            if (focusVersionFinder.isCorrect()) {
-                result.versions.put(App.FIREFOX_FOCUS, focusVersionFinder.getVersion());
-                result.downloadUrl.put(App.FIREFOX_FOCUS, focusVersionFinder.getDownloadUrl(App.FIREFOX_FOCUS, DeviceABI.getAbi()));
-
-                result.versions.put(App.FIREFOX_KLAR, focusVersionFinder.getVersion());
-                result.downloadUrl.put(App.FIREFOX_KLAR, focusVersionFinder.getDownloadUrl(App.FIREFOX_KLAR, DeviceABI.getAbi()));
-            }
+        if (appsToCheck.contains(FIREFOX_KLAR) ||
+                appsToCheck.contains(FIREFOX_FOCUS)) {
+            checkFocusKlar(result, appsToCheck);
         }
 
-        if (appsToCheck.contains(App.FIREFOX_LITE)) {
-            FirefoxLiteVersionFinder firefoxLiteVersionFinder = FirefoxLiteVersionFinder.create();
-            if (firefoxLiteVersionFinder.isCorrect()) {
-                result.versions.put(App.FIREFOX_LITE, firefoxLiteVersionFinder.getVersion());
-                result.downloadUrl.put(App.FIREFOX_LITE, firefoxLiteVersionFinder.getDownloadUrl());
-            }
+        if (appsToCheck.contains(FIREFOX_LITE)) {
+            checkLite(result);
         }
 
-        if (appsToCheck.contains(App.FENIX)) {
-            FenixVersionFinder fenixVersionFinder = FenixVersionFinder.create();
-            if (fenixVersionFinder.isCorrect()) {
-                result.versions.put(App.FENIX, fenixVersionFinder.getVersion());
-                result.downloadUrl.put(App.FENIX, fenixVersionFinder.getDownloadUrl(DeviceABI.getAbi()));
-            }
+        if (appsToCheck.contains(FENIX)) {
+            checkFenix(result);
         }
         return result;
     }
+
+    private static void checkFennec(AvailableApps result, Set<App> appsToCheck) {
+        Optional<OfficialApi.Version> response = OfficialApi.getResponse();
+        if (!response.isPresent()) {
+            return;
+        }
+
+        OfficialApi.Version version = response.get();
+        if (appsToCheck.contains(FENNEC_RELEASE)) {
+            result.versions.put(FENNEC_RELEASE, version.getReleaseVersion());
+            result.downloadUrl.put(FENNEC_RELEASE, getDownloadUrlForFennec(FENNEC_RELEASE, version));
+        }
+        if (appsToCheck.contains(FENNEC_BETA)) {
+            result.versions.put(FENNEC_BETA, version.getBetaVersion());
+            result.downloadUrl.put(FENNEC_BETA, getDownloadUrlForFennec(FENNEC_BETA, version));
+        }
+        if (appsToCheck.contains(FENNEC_NIGHTLY)) {
+            result.versions.put(FENNEC_NIGHTLY, version.getNightlyVersion());
+            result.downloadUrl.put(FENNEC_NIGHTLY, getDownloadUrlForFennec(FENNEC_NIGHTLY, version));
+        }
+    }
+
+    private static void checkFocusKlar(AvailableApps result, Set<App> appsToCheck) {
+        FocusVersionFinder focusVersionFinder = FocusVersionFinder.create();
+        if (!focusVersionFinder.isCorrect()) {
+            return;
+        }
+
+        if (appsToCheck.contains(FIREFOX_FOCUS)) {
+            result.versions.put(FIREFOX_FOCUS, focusVersionFinder.getVersion());
+            result.downloadUrl.put(FIREFOX_FOCUS, focusVersionFinder.getDownloadUrl(FIREFOX_FOCUS, DeviceABI.getAbi()));
+        }
+
+        if (appsToCheck.contains(FIREFOX_KLAR)) {
+            result.versions.put(FIREFOX_KLAR, focusVersionFinder.getVersion());
+            result.downloadUrl.put(FIREFOX_KLAR, focusVersionFinder.getDownloadUrl(FIREFOX_KLAR, DeviceABI.getAbi()));
+        }
+    }
+
+    private static void checkLite(AvailableApps result) {
+        FirefoxLiteVersionFinder firefoxLiteVersionFinder = FirefoxLiteVersionFinder.create();
+        if (!firefoxLiteVersionFinder.isCorrect()) {
+            return;
+        }
+
+        result.versions.put(FIREFOX_LITE, firefoxLiteVersionFinder.getVersion());
+        result.downloadUrl.put(FIREFOX_LITE, firefoxLiteVersionFinder.getDownloadUrl());
+    }
+
+    private static void checkFenix(AvailableApps result) {
+        FenixVersionFinder fenixVersionFinder = FenixVersionFinder.create();
+        if (!fenixVersionFinder.isCorrect()) {
+            return;
+        }
+
+        result.versions.put(FENIX, fenixVersionFinder.getVersion());
+        result.downloadUrl.put(FENIX, fenixVersionFinder.getDownloadUrl(DeviceABI.getAbi()));
+    }
+
 
     /**
      * Internal method for retrieving the download url for FENNEC_RELEASE, FENNEC_BETA, FENNEC_NIGHTLY.
      * Reason: There are two methods for retrieving the url.
      * {@link MozillaFtp} works better but can fail. If it fails, then {@link OfficialApi} will be used.
+     *
      * @param app
      * @param version
      * @return
@@ -121,6 +162,7 @@ public class AvailableApps {
     /**
      * Get the version name for the given app - if the app was in {@code appsToCheck} in the method
      * {@code create()}
+     *
      * @param app
      * @return version name
      */
@@ -130,6 +172,7 @@ public class AvailableApps {
 
     /**
      * Check if for a given app and a given installed version a newer version is available.
+     *
      * @param app
      * @param installedVersion
      * @return is update available
@@ -140,11 +183,11 @@ public class AvailableApps {
             return false;
         }
 
-        if (app == App.FENNEC_BETA) {
+        if (app == FENNEC_BETA) {
             String sanitizedAvailable = availableVersion.get().split("b")[0];
             return !sanitizedAvailable.contentEquals(installedVersion);
         }
-        if (app == App.FIREFOX_LITE) {
+        if (app == FIREFOX_LITE) {
             String sanitizedInstalled = installedVersion.split("\\(")[0];
             return !availableVersion.get().contentEquals(sanitizedInstalled);
         }
@@ -155,6 +198,7 @@ public class AvailableApps {
     /**
      * Get the download url for the given app - if the app was in {@code appsToCheck} in the method
      * {@code create()}
+     *
      * @param app
      * @return download url
      */
@@ -164,6 +208,7 @@ public class AvailableApps {
 
     /**
      * Should an app download triggered?
+     *
      * @return
      */
     boolean isTriggerDownload() {
@@ -172,6 +217,7 @@ public class AvailableApps {
 
     /**
      * Which app should be downloaded?
+     *
      * @return
      */
     App getAppToDownload() {
