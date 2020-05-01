852aa18a4bad42d5df668a8b9c4d37b0af67d11d
Tobiwan <tobiwanop@posteo.de>
2020-05-01 13:35:32 +0200

    - cleanup and javadoc to GsonApiConsumer.java

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/version/GsonApiConsumer.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/version/GsonApiConsumer.java
@@ -14,17 +14,24 @@ import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
 
 /**
- * Created by Tobiwan on 05.04.2020.
+ * Consume a REST-API from the internet.
  */
 class GsonApiConsumer {
     private static final String LOG_TAG = "ffupdater";
     private static final int TIMEOUT = 5000;
 
+    /**
+     * Download the JSON respond from url and convert the JSON result to a Java object.
+     * @param url url
+     * @param clazz JSON result will be parsed to an instance of this clazz
+     * @param <T> clazz
+     * @return result or null (if an error occurs)
+     */
     @Nullable
-    static <T> T consume(String urlString, Class<T> clazz) {
+    static <T> T consume(String url, Class<T> clazz) {
         HttpsURLConnection urlConnection;
         try {
-            urlConnection = (HttpsURLConnection) new URL(urlString).openConnection();
+            urlConnection = (HttpsURLConnection) new URL(url).openConnection();
         } catch (IOException e) {
             Log.e(LOG_TAG, "Can't connect to API interface", e);
             return null;
