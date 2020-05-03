cb056206b77afc6e04579296acae9211d7c3a1aa
Tobiwan <tobiwanop@posteo.de>
2020-05-03 17:47:02 +0200

    - move code for converting the API Level to the Android version + codename

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/utils/Utils.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/utils/Utils.java
@@ -14,7 +14,7 @@ public class Utils {
     /**
      * @param string string
      * @return if string is null, then return empty string.
-     *         if string is not null, return string.
+     * if string is not null, return string.
      */
     @NonNull
     public static String convertNullToEmptyString(@Nullable String string) {
@@ -33,5 +33,47 @@ public class Utils {
         } catch (InterruptedException e) {
             Log.e(LOG_TAG, "failed sleep", e);
         }
+    }
+
+    private static final String[] versionAndCodenames = new String[]{
+            "1.0",
+            "1.1",
+            "1.5 Cupcake",
+            "1.6 (Donut)",
+            "2.0 (Eclair)",
+            "2.0.1 (Eclair)",
+            "2.1 (Eclair)",
+            "2.2 (Froyo)",
+            "2.3 (Gingerbread)",
+            "2.3.3 (Gingerbread)",
+            "3.0 (Honeycomb)",
+            "3.1 (Honeycomb)",
+            "3.2 (Honeycomb)",
+            "4.0.1 (Ice Cream Sandwich)",
+            "4.0.3 (Ice Cream Sandwich)",
+            "4.1 (Jelly Bean)",
+            "4.2 (Jelly Bean)",
+            "4.3 (Jelly Bean)",
+            "4.4 (KitKat)",
+            "5.0 (Lollipop)",
+            "5.1 (Lollipop)",
+            "6.0 (Marshmallow)",
+            "7.0 (Nougat)",
+            "7.1 (Nougat)",
+            "8.0.0 (Oreo)",
+            "8.1.0 (Oreo)",
+            "9 (Pie)",
+            "10 (Android10)",
+    };
+
+    /**
+     * @param apiLevel API Level
+     * @return the Android version an its codename for the associated API Level
+     */
+    public static String getVersionAndCodenameFromApiLevel(int apiLevel) {
+        if (apiLevel <= 0 || apiLevel >= versionAndCodenames.length) {
+            return "API Level " + apiLevel;
+        }
+        return versionAndCodenames[apiLevel - 1];
     }
 }
