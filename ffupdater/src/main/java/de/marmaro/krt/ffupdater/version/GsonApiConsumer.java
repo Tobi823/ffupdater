b9f2a974396b6ffbab584c2f323371c75a5d93d8
Tobi823 <Tobi823@users.noreply.github.com>
2020-05-31 15:51:52 +0200

    Tobiwan/ffupdater#27 - support GZIP payload compression when calling APIs because the API of the Mozilla CI server requires it - rename Gson

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/version/GsonApiConsumer.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/version/GsonApiConsumer.java
@@ -4,12 +4,15 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import com.google.common.base.Preconditions;
 import com.google.gson.Gson;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.zip.GZIPInputStream;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -20,12 +23,20 @@ import javax.net.ssl.HttpsURLConnection;
 class GsonApiConsumer {
     private static final String LOG_TAG = "ffupdater";
     private static final int TIMEOUT = 5000;
+    public static final String GZIP = "gzip";
+    public static final String ACCEPT_ENCODING = "Accept-Encoding";
 
     /**
      * Download the JSON respond from url and convert the JSON result to a Java object.
-     * @param url url
+     *
+     * Request the server for gzip-compression. <a href="https://stackoverflow.com/a/27720902">Link</a>
+     * Cons: This may help an attacker to guess the plaintext of the encrypted HTTPS connection due
+     * to gzip. But the plaintext is not secret because everyone can access it by calling the URLs.
+     * Pros: Saves network traffic.
+     *
+     * @param url   url
      * @param clazz JSON result will be parsed to an instance of this clazz
-     * @param <T> clazz
+     * @param <T>   clazz
      * @return result or null (if an error occurs)
      */
     @Nullable
@@ -37,9 +48,17 @@ class GsonApiConsumer {
             Log.e(LOG_TAG, "Can't connect to API interface", e);
             return null;
         }
+        urlConnection.setRequestProperty(ACCEPT_ENCODING, GZIP);
         urlConnection.setConnectTimeout(TIMEOUT);
         urlConnection.setReadTimeout(TIMEOUT);
-        try (InputStreamReader reader = new InputStreamReader(new BufferedInputStream(urlConnection.getInputStream()))) {
+
+        String contentEncoding = urlConnection.getContentEncoding();
+        boolean gzipped = GZIP.equals(contentEncoding);
+        Preconditions.checkArgument(gzipped || contentEncoding == null);
+
+        try (InputStream buffered = new BufferedInputStream(urlConnection.getInputStream());
+             InputStream decompressed = gzipped ? new GZIPInputStream(buffered) : buffered;
+             InputStreamReader reader = new InputStreamReader(decompressed)) {
             return new Gson().fromJson(reader, clazz);
         } catch (IOException e) {
             Log.e(LOG_TAG, "Can't consume API interface", e);
