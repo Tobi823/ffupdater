117026f8aaa0a43759e14f55d594aa7a932b4f2c
Tobi823 <Tobi823@users.noreply.github.com>
2020-10-25 18:03:31 +0100

    - improve notification when background error occurs

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/notification/ErrorNotificationManager.java
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/notification/ErrorNotificationManager.java
@@ -4,15 +4,10 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.graphics.BitmapFactory;
 import android.os.Build;
-import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
@@ -22,6 +17,7 @@ import java.io.StringWriter;
 import java.util.Objects;
 
 import de.marmaro.krt.ffupdater.R;
+import james.crasher.activities.CrashActivity;
 
 import static android.app.NotificationManager.IMPORTANCE_DEFAULT;
 import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
@@ -30,7 +26,6 @@ import static de.marmaro.krt.ffupdater.R.mipmap.transparent;
 public class ErrorNotificationManager {
     public static final String CHANNEL_ID = "error_notification_channel";
     public static final int NOTIFICATION_ID = 300;
-    public static final String COPY_TO_CLIPBOARD_INTENT = "de.marmaro.krt.ffupdater.notification.ErrorNotificationManager.ACTION_COPY";
 
     private final Context context;
     private final NotificationManager notificationManager;
@@ -41,29 +36,21 @@ public class ErrorNotificationManager {
     }
 
     public void showNotification(Exception e) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
+        StringWriter stackTrace = new StringWriter();
+        e.printStackTrace(new PrintWriter(stackTrace));
 
-        final BroadcastReceiver copyToClipboard = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                ClipboardManager clipboard = (ClipboardManager)
-                        context.getSystemService(Context.CLIPBOARD_SERVICE);
-                clipboard.setPrimaryClip(ClipData.newPlainText("FFUpdater error", sw.toString()));
-                Toast.makeText(context, "Copied!", Toast.LENGTH_SHORT).show();
-            }
-        };
+        final Intent intent = new Intent(context, CrashActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(CrashActivity.EXTRA_NAME, e.getClass().getName());
+        intent.putExtra(CrashActivity.EXTRA_MESSAGE, e.getLocalizedMessage());
+        intent.putExtra(CrashActivity.EXTRA_STACK_TRACE, stackTrace.toString());
 
-        context.registerReceiver(copyToClipboard, new IntentFilter(COPY_TO_CLIPBOARD_INTENT));
-        final Intent copyIntent = new Intent(COPY_TO_CLIPBOARD_INTENT);
-        final PendingIntent updateAppIntent = PendingIntent.getBroadcast(context, 0, copyIntent, FLAG_UPDATE_CURRENT);
-
+        final PendingIntent updateAppIntent = PendingIntent.getActivity(context, 0, intent, FLAG_UPDATE_CURRENT);
         final Notification notification = createNotificationBuilder()
                 .setSmallIcon(transparent, 0)
                 .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
                 .setContentTitle("Background update check failed")
-                .setContentText("Error: " + sw.toString())
+                .setContentText("Click to view error")
                 .setContentIntent(updateAppIntent)
                 .setAutoCancel(true)
                 .build();
