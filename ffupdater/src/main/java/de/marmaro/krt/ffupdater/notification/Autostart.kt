7e0b65cf9c482fc57f843332688ae3a803c61f97
Tobi823 <Tobi823@users.noreply.github.com>
2021-02-08 19:25:47 +0100

    Tobi823/ffupdater#17
    - improve Autostart.kt

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/notification/Autostart.kt
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/notification/Autostart.kt
@@ -1,20 +1,17 @@
-package de.marmaro.krt.ffupdater.notification;
+package de.marmaro.krt.ffupdater.notification
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-
-import static android.content.Intent.ACTION_BOOT_COMPLETED;
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 
 /**
- * After the phone is booted, this class will start the {@link BackgroundUpdateChecker}.
+ * After the phone is booted, this class will start the [BackgroundUpdateChecker].
  * Reason: FFUpdater should be able to check for updates even after a phone reboot.
  */
-public class Autostart extends BroadcastReceiver {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (intent != null && ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            new BackgroundUpdateCheckerCreator(context).startOrStopBackgroundUpdateCheck();
+class Autostart : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+            BackgroundUpdateCheckerCreator(context).startOrStopBackgroundUpdateCheck()
         }
     }
 }
