ceccbf10dfffd1c409c7d54c182f26ba6f222d60
Tobi823 <Tobi823@users.noreply.github.com>
2021-02-13 17:46:00 +0100

    Tobi823/ffupdater#17
    - add unit tests for Iceraven

---
+++ ffupdater/src/test/java/de/marmaro/krt/ffupdater/app/impl/IcravenIT.kt
@@ -0,0 +1,123 @@
+package de.marmaro.krt.ffupdater.app.impl
+
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Build
+import com.google.gson.Gson
+import de.marmaro.krt.ffupdater.app.App
+import de.marmaro.krt.ffupdater.app.impl.fetch.ApiConsumer
+import de.marmaro.krt.ffupdater.app.impl.fetch.github.GithubConsumer
+import de.marmaro.krt.ffupdater.device.ABI
+import de.marmaro.krt.ffupdater.device.DeviceEnvironment
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import java.io.File
+import java.net.URL
+
+class IcravenIT {
+
+    @MockK
+    lateinit var apiConsumer: ApiConsumer
+
+    @MockK
+    lateinit var context: Context
+
+    @MockK
+    lateinit var packageManager: PackageManager
+
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
+        every { context.packageManager } returns packageManager
+    }
+
+    @Test
+    fun updateCheckBlocking_latestRelease_checkDownloadUrlForABI() {
+        val path = "src/test/resources/de/marmaro/krt/ffupdater/app/impl/Iceraven/latest.json"
+        val url = "https://api.github.com/repos/fork-maintainers/iceraven-browser/releases/latest"
+        every { apiConsumer.consume(URL(url), GithubConsumer.Release::class.java) } returns
+                Gson().fromJson(File(path).readText(), GithubConsumer.Release::class.java)
+
+        val packageInfo = PackageInfo()
+        packageInfo.versionName = "1.19.92"
+        every {
+            packageManager.getPackageInfo(App.ICERAVEN.detail.packageName, 0)
+        } returns packageInfo
+
+        runBlocking {
+            val abi = ABI.ARMEABI_V7A
+            val deviceEnvironment = DeviceEnvironment(listOf(abi), Build.VERSION_CODES.R)
+            val actual = Iceraven(apiConsumer).updateCheck(context, deviceEnvironment).downloadUrl
+            val expected = "https://github.com/fork-maintainers/iceraven-browser/releases/" +
+                    "download/iceraven-1.6.0/iceraven-1.6.0-browser-armeabi-v7a-forkRelease.apk"
+            assertEquals(URL(expected), actual)
+        }
+
+        runBlocking {
+            val abi = ABI.ARM64_V8A
+            val deviceEnvironment = DeviceEnvironment(listOf(abi), Build.VERSION_CODES.R)
+            val actual = Iceraven(apiConsumer).updateCheck(context, deviceEnvironment).downloadUrl
+            val expected = "https://github.com/fork-maintainers/iceraven-browser/releases/" +
+                    "download/iceraven-1.6.0/iceraven-1.6.0-browser-arm64-v8a-forkRelease.apk"
+            assertEquals(URL(expected), actual)
+        }
+
+        runBlocking {
+            val abi = ABI.X86
+            val deviceEnvironment = DeviceEnvironment(listOf(abi), Build.VERSION_CODES.R)
+            val actual = Iceraven(apiConsumer).updateCheck(context, deviceEnvironment).downloadUrl
+            val expected = "https://github.com/fork-maintainers/iceraven-browser/releases/" +
+                    "download/iceraven-1.6.0/iceraven-1.6.0-browser-x86-forkRelease.apk"
+            assertEquals(URL(expected), actual)
+        }
+
+        runBlocking {
+            val abi = ABI.X86_64
+            val deviceEnvironment = DeviceEnvironment(listOf(abi), Build.VERSION_CODES.R)
+            val actual = Iceraven(apiConsumer).updateCheck(context, deviceEnvironment).downloadUrl
+            val expected = "https://github.com/fork-maintainers/iceraven-browser/releases/" +
+                    "download/iceraven-1.6.0/iceraven-1.6.0-browser-x86_64-forkRelease.apk"
+            assertEquals(URL(expected), actual)
+        }
+    }
+
+    @Test
+    fun updateCheckBlocking_latestRelease_updateCheck() {
+        val path = "src/test/resources/de/marmaro/krt/ffupdater/app/impl/Iceraven/latest.json"
+        val url = "https://api.github.com/repos/fork-maintainers/iceraven-browser/releases/latest"
+        every { apiConsumer.consume(URL(url), GithubConsumer.Release::class.java) } returns
+                Gson().fromJson(File(path).readText(), GithubConsumer.Release::class.java)
+
+        val packageInfo = PackageInfo()
+        every {
+            packageManager.getPackageInfo(App.ICERAVEN.detail.packageName, 0)
+        } returns packageInfo
+
+        val deviceEnvironment = DeviceEnvironment(listOf(ABI.ARMEABI_V7A), Build.VERSION_CODES.R)
+
+        // installed app is up-to-date
+        runBlocking {
+            packageInfo.versionName = "1.6.0"
+            val actual = Iceraven(apiConsumer).updateCheck(context, deviceEnvironment)
+            assertFalse(actual.isUpdateAvailable)
+            assertEquals("1.6.0", actual.version)
+            assertEquals(66150140L, actual.fileSizeBytes)
+        }
+
+        // installed app is old
+        runBlocking {
+            packageInfo.versionName = "1.5.0"
+            val actual = Iceraven(apiConsumer).updateCheck(context, deviceEnvironment)
+            assertTrue(actual.isUpdateAvailable)
+            assertEquals("1.6.0", actual.version)
+            assertEquals(66150140L, actual.fileSizeBytes)
+        }
+    }
+}
