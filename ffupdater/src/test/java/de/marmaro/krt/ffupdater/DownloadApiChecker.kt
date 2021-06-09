e703da2f1102bf22626f02fe36041c8fba2e0835
Tobi823 <Tobi823@users.noreply.github.com>
2021-06-10 00:28:41 +0200

    Increase max time without update for Lockwise in test

--- ffupdater/src/test/java/de/marmaro/krt/ffupdater/DownloadApiChecker.kt
+++ ffupdater/src/test/java/de/marmaro/krt/ffupdater/DownloadApiChecker.kt
@@ -155,7 +155,7 @@ class DownloadApiChecker {
         }
         verifyThatDownloadLinkAvailable(result.downloadUrl)
         val age = Duration.between(result.publishDate, ZonedDateTime.now())
-        val maxDays = 180
+        val maxDays = 7 * 30
         assertTrue("$age must be smaller then $maxDays days", age.toDays() < maxDays)
     }
 
