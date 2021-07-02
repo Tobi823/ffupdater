59bbcfcddf65338ae4f5cafae9528121cb75f316
Tobi823 <Tobi823@users.noreply.github.com>
2021-07-02 09:52:55 +0200

    adjust unit test for Firefox Release

--- ffupdater/src/test/java/de/marmaro/krt/ffupdater/DownloadApiChecker.kt
+++ ffupdater/src/test/java/de/marmaro/krt/ffupdater/DownloadApiChecker.kt
@@ -133,7 +133,7 @@ class DownloadApiChecker {
         }
         verifyThatDownloadLinkAvailable(result.downloadUrl)
         val age = Duration.between(result.publishDate, ZonedDateTime.now())
-        val maxDays = 30
+        val maxDays = 42
         assertTrue("$age must be smaller then $maxDays days", age.toDays() < maxDays)
     }
 
