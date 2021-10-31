978e948f727d6c75f66ad890e47679c9c3f47a96
Tobi823 <Tobi823@users.noreply.github.com>
2021-10-31 13:09:31 +0100

    - change internet check:   - use the old API `ConnectivityManager#getActiveNetworkInfo()` for API < 29   - use the new API `capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)` for API >= 29   - do not use DNS requests for internet checking   - why? Android WorkManager still uses the old API (and I hope that the internet checking in Android WorkManager is more reliable https://android.googlesource.com/platform/frameworks/support.git/+/refs/heads/androidx-main/work/work-runtime/src/main/java/androidx/work/impl/constraints/trackers/NetworkStateTracker.java#135)

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/download/NetworkUtil.kt
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/download/NetworkUtil.kt
@@ -4,42 +4,31 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
-import androidx.annotation.MainThread
 import androidx.annotation.RequiresApi
 import de.marmaro.krt.ffupdater.device.DeviceEnvironment
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.net.InetAddress
-import java.net.UnknownHostException
 
 object NetworkUtil {
-    private const val GITHUB_API_HOST = "api.github.com"
-
-    @MainThread
-    suspend fun isInternetUnavailable(context: Context): Boolean {
+    fun isInternetUnavailable(context: Context): Boolean {
         return !isInternetAvailable(context)
     }
 
-    @MainThread
-    suspend fun isInternetAvailable(context: Context): Boolean {
+    fun isInternetAvailable(context: Context): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         return isInternetAvailable(cm)
     }
 
-    @MainThread
-    private suspend fun isInternetAvailable(cm: ConnectivityManager): Boolean {
-        val networkConnected = if (DeviceEnvironment.supportsAndroidMarshmallow()) {
+    private fun isInternetAvailable(cm: ConnectivityManager): Boolean {
+        return if (DeviceEnvironment.supportsAndroid10()) {
             isNetworkConnected(cm)
         } else {
             isNetworkConnectedOldWay(cm)
         }
-        return networkConnected && isDnsResolvable()
     }
 
     /**
      * https://gist.github.com/Farbklex/f84029889444ee9c52a331a7e2bd10d2
      */
-    @RequiresApi(Build.VERSION_CODES.M)
+    @RequiresApi(Build.VERSION_CODES.Q)
     private fun isNetworkConnected(cm: ConnectivityManager): Boolean {
         val activeNetwork = cm.activeNetwork ?: return false
         val capabilities = cm.getNetworkCapabilities(activeNetwork) ?: return false
@@ -47,22 +36,14 @@ object NetworkUtil {
                 capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
     }
 
+    /**
+     * How AndroidX WorkManager checks for internet access:
+     * https://android.googlesource.com/platform/frameworks/support.git/+/refs/heads/androidx-main/
+     * work/work-runtime/src/main/java/androidx/work/impl/constraints/trackers/NetworkStateTracker.java#135
+     */
     @Suppress("DEPRECATION")
     private fun isNetworkConnectedOldWay(cm: ConnectivityManager): Boolean {
         return cm.activeNetworkInfo?.isConnected == true
-    }
-
-    @Suppress("BlockingMethodInNonBlockingContext")
-    @MainThread
-    private suspend fun isDnsResolvable(): Boolean {
-        return withContext(Dispatchers.IO) {
-            try {
-                val address = InetAddress.getByName(GITHUB_API_HOST)
-                address.hostAddress?.isNotEmpty() == true
-            } catch (e: UnknownHostException) {
-                false
-            }
-        }
     }
 
     fun isActiveNetworkUnmetered(context: Context): Boolean {
