18ad7ea569d4979373cfc20e733cb8de96b54507
Tobi823 <Tobi823@users.noreply.github.com>
2021-11-10 15:07:13 +0100

    - refactor PackageManagerUtil.kt to make it less prone to misuse

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/download/PackageManagerUtil.kt
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/download/PackageManagerUtil.kt
@@ -6,9 +6,7 @@ import android.content.pm.PackageManager
 import android.content.pm.PackageManager.GET_SIGNATURES
 import android.content.pm.PackageManager.GET_SIGNING_CERTIFICATES
 import android.content.pm.Signature
-import android.os.Build
 import androidx.annotation.MainThread
-import androidx.annotation.RequiresApi
 import de.marmaro.krt.ffupdater.app.App
 import de.marmaro.krt.ffupdater.device.DeviceEnvironment
 import kotlinx.coroutines.Dispatchers
@@ -17,36 +15,38 @@ import kotlinx.coroutines.withContext
 object PackageManagerUtil {
 
     @MainThread
-    suspend fun getPackageArchiveInfo(packageManager: PackageManager, path: String): Signature? {
+    suspend fun getPackageArchiveInfo(packageManager: PackageManager, path: String): Signature {
         return withContext(Dispatchers.IO) {
-            if (DeviceEnvironment.supportsAndroid11()) {
+            val packageInfo = if (DeviceEnvironment.supportsAndroid11()) {
                 // GET_SIGNING_CERTIFICATES does not work on Android 9/10
                 val packageInfo = packageManager.getPackageArchiveInfo(path, GET_SIGNING_CERTIFICATES)
-                extractSignatureForNewerDevices(packageInfo)
+                checkNotNull(packageInfo) { "PackageInfo for file '$packageInfo' is null." }
             } else {
                 val packageInfo = packageManager.getPackageArchiveInfo(path, GET_SIGNATURES)
-                extractSignatureForOlderDevices(packageInfo)
+                checkNotNull(packageInfo) { "PackageInfo for file '$packageInfo' is null." }
             }
+            extractSignature(packageInfo)
         }
     }
 
     @MainThread
-    suspend fun getPackageArchiveVersionName(packageManager: PackageManager, path: String): String? {
+    suspend fun getPackageArchiveVersionNameOrNull(packageManager: PackageManager, path: String): String? {
         return withContext(Dispatchers.IO) {
             packageManager.getPackageArchiveInfo(path, 0)?.versionName
         }
     }
 
-    fun getInstalledAppInfo(packageManager: PackageManager, app: App): Signature? {
-        return if (DeviceEnvironment.supportsAndroid9()) {
+    fun getInstalledAppInfo(packageManager: PackageManager, app: App): Signature {
+        val packageInfo = if (DeviceEnvironment.supportsAndroid9()) {
             val packageInfo = packageManager.getPackageInfo(app.detail.packageName, GET_SIGNING_CERTIFICATES)
-            extractSignatureForNewerDevices(packageInfo)
+            checkNotNull(packageInfo) { "PackageInfo for package ${app.detail.packageName} is null." }
         } else {
             @SuppressLint("PackageManagerGetSignatures")
             // because GET_SIGNATURES is dangerous on Android 4.4 or lower https://stackoverflow.com/a/39348300
             val packageInfo = packageManager.getPackageInfo(app.detail.packageName, GET_SIGNATURES)
-            extractSignatureForOlderDevices(packageInfo)
+            checkNotNull(packageInfo) { "PackageInfo for package ${app.detail.packageName} is null." }
         }
+        return extractSignature(packageInfo)
     }
 
     fun getInstalledAppVersionName(packageManager: PackageManager, packageName: String): String? {
@@ -66,21 +66,22 @@ object PackageManagerUtil {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.P)
-    private fun extractSignatureForNewerDevices(packageInfo: PackageInfo?): Signature? {
-        val signingInfo = (packageInfo ?: return null).signingInfo
-        checkNotNull(signingInfo) { "PackageInfo#signingInfo is null" }
-        check(!signingInfo.hasMultipleSigners()) { "App ${packageInfo.packageName} has multiple signers" }
-        check(signingInfo.signingCertificateHistory.size == 1) {
-            "App ${packageInfo.packageName} has ${signingInfo.signingCertificateHistory.size} certificates"
+    private fun extractSignature(packageInfo: PackageInfo): Signature {
+        if (DeviceEnvironment.supportsAndroid9() && packageInfo.signingInfo != null) {
+            val signingInfo = packageInfo.signingInfo
+            check(!signingInfo.hasMultipleSigners()) { "App ${packageInfo.packageName} has multiple signers" }
+            check(signingInfo.signingCertificateHistory.size == 1) {
+                "App ${packageInfo.packageName} has ${signingInfo.signingCertificateHistory.size} certificates."
+            }
+            return signingInfo.signingCertificateHistory[0]
         }
-        return signingInfo.signingCertificateHistory[0]
-    }
-
-    private fun extractSignatureForOlderDevices(packageInfo: PackageInfo?): Signature? {
-        val signatures = (packageInfo ?: return null).signatures
-        checkNotNull(signatures) { "PackageInfo#signatures is null" }
-        check(signatures.size == 1) { "App ${packageInfo.packageName} has ${signatures.size} signatures" }
-        return signatures[0]
+        if (packageInfo.signatures != null) {
+            val signatures = packageInfo.signatures
+            check(signatures.size == 1) {
+                "App ${packageInfo.packageName} has ${signatures.size} signatures."
+            }
+            return signatures[0]
+        }
+        throw IllegalArgumentException("PackageInfo has no signingInfo and no signatures.")
     }
 }
