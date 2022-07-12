5c966018b4f94102eb2ec735c16a2619c8a6f170
Tobi823 <ffupdater@mailbox.org>
2022-07-12 11:40:06 +0200

    * store eol info in the main app class

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/EolAppsWarnerReceiver.kt
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/EolAppsWarnerReceiver.kt
@@ -3,20 +3,23 @@ package de.marmaro.krt.ffupdater
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import de.marmaro.krt.ffupdater.app.EolApp
+import de.marmaro.krt.ffupdater.app.MaintainedApp
 import de.marmaro.krt.ffupdater.notification.BackgroundNotificationBuilder
+import de.marmaro.krt.ffupdater.utils.ifTrue
 
 
 class EolAppsWarnerReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent) {
-        require(intent.action == Intent.ACTION_MY_PACKAGE_REPLACED)
-        require(intent.`package` == BuildConfig.APPLICATION_ID)
-        requireNotNull(context)
-
-        val eolAppsInstalled = EolApp.values()
-            .any { it.impl.isInstalled(context) }
-        if (eolAppsInstalled) {
-            BackgroundNotificationBuilder.showEolAppsWarning(context)
+        if (context == null ||
+            intent.action != Intent.ACTION_MY_PACKAGE_REPLACED ||
+            intent.`package` != BuildConfig.APPLICATION_ID
+        ) {
+            return
         }
+
+        MaintainedApp.values()
+            .filter { app -> app.detail.isEol() }
+            .any { app -> app.detail.isInstalled(context) }
+            .ifTrue { BackgroundNotificationBuilder.showEolAppsWarning(context) }
     }
 }
