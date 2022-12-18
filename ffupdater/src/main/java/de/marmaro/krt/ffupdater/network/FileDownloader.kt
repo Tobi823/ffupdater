a19aff47f2c3c2b2aa05439a5f3339b6b4c84be2
Tobi823 <ffupdater@mailbox.org>
2022-12-18 23:03:36 +0100

    - refactor

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/network/FileDownloader.kt
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/network/FileDownloader.kt
@@ -11,6 +11,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.dnsoverhttps.DnsOverHttps
 import okio.Buffer
 import okio.ForwardingSource
 import okio.Source
@@ -18,7 +20,13 @@ import okio.buffer
 import ru.gildor.coroutines.okhttp.await
 import java.io.File
 import java.io.IOException
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.Proxy
+import java.security.KeyStore
+import java.security.SecureRandom
 import java.util.concurrent.atomic.AtomicInteger
+import javax.net.ssl.*
 import kotlin.reflect.KMutableProperty0
 
 
@@ -136,20 +144,74 @@ class FileDownloader(private val networkSettingsHelper: NetworkSettingsHelper) {
                 .build()
         }
 
-        networkSettingsHelper.createSslSocketFactory()?.let {
+        createSslSocketFactory()?.let {
             builder.sslSocketFactory(it.first, it.second)
         }
-        networkSettingsHelper.createDnsConfiguration()?.let {
+        createDnsConfiguration()?.let {
             builder.dns(it)
         }
-        networkSettingsHelper.createProxyConfiguration()?.let {
+        createProxyConfiguration()?.let {
             builder.proxy(it)
         }
-        networkSettingsHelper.createProxyAuthenticatorConfiguration()?.let {
+        createProxyAuthenticatorConfiguration()?.let {
             builder.proxyAuthenticator(it)
         }
 
         return builder.build()
+    }
+
+    private fun createSslSocketFactory(): Pair<SSLSocketFactory, X509TrustManager>? {
+        if (!networkSettingsHelper.areUserCAsTrusted) {
+            return null // use the system trust manager
+        }
+
+        // https://developer.android.com/reference/java/security/KeyStore
+        val systemAndUserCAStore = KeyStore.getInstance("AndroidCAStore")
+        systemAndUserCAStore.load(null)
+
+        // https://stackoverflow.com/a/24401795
+        // https://github.com/bitfireAT/davx5-ose/blob/0e93a47d6d7277d3a18e31c6528f578c467a56ea/app/src/main/java/at/bitfire/davdroid/HttpClient.kt
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        trustManagerFactory.init(systemAndUserCAStore)
+
+        val keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm())
+        keyManagerFactory.init(systemAndUserCAStore, "keystore_pass".toCharArray())
+
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(keyManagerFactory.keyManagers, trustManagerFactory.trustManagers, SecureRandom())
+
+        val trustManager = trustManagerFactory.trustManagers.first() as X509TrustManager
+        return Pair(sslContext.socketFactory, trustManager)
+    }
+
+    private fun createDnsConfiguration(): Dns? {
+        return when (networkSettingsHelper.dnsProvider) {
+            NetworkSettingsHelper.DnsProvider.SYSTEM -> null
+            NetworkSettingsHelper.DnsProvider.DIGITAL_SOCIETY_SWITZERLAND_DOH -> digitalSocietySwitzerlandDoH
+            NetworkSettingsHelper.DnsProvider.QUAD9_DOH -> quad9DoH
+            NetworkSettingsHelper.DnsProvider.CLOUDFLARE_DOH -> cloudflareDoH
+            NetworkSettingsHelper.DnsProvider.GOOGLE_DOH -> googleDoH
+            NetworkSettingsHelper.DnsProvider.CUSTOM_SERVER -> createDnsConfigurationFromUserInput()
+            NetworkSettingsHelper.DnsProvider.NO -> fakeDnsResolver
+        }
+    }
+
+    private fun createDnsConfigurationFromUserInput(): DnsOverHttps {
+        val customServer = networkSettingsHelper.customDohServer()
+        return createDnsOverHttpsResolver(customServer.host, customServer.ips)
+    }
+
+    private fun createProxyConfiguration(): Proxy? {
+        val proxy = networkSettingsHelper.proxy() ?: return null
+        return Proxy(proxy.type, InetSocketAddress(proxy.host, proxy.port))
+    }
+
+    private fun createProxyAuthenticatorConfiguration(): Authenticator? {
+        val proxy = networkSettingsHelper.proxy() ?: return null
+        val username = proxy.username ?: return null
+        val password = proxy.password
+            ?: throw IllegalArgumentException("Invalid proxy configuration. You have to specify a password.")
+        return ProxyAuthenticator(username, password)
     }
 
     // simple communication between WorkManager and the InstallActivity to prevent duplicated downloads
@@ -161,6 +223,64 @@ class FileDownloader(private val networkSettingsHelper: NetworkSettingsHelper) {
                 ((System.currentTimeMillis() - lastChange) < 3600_000)
 
         const val GITHUB_URL = "https://api.github.com"
+
+        // https://github.com/square/okhttp/blob/7768de7baaa992adcd384871cb8720873f6b8fd0/okhttp-dnsoverhttps/src/test/java/okhttp3/dnsoverhttps/DnsOverHttpsTest.java
+        // sharing an OkHttpClient is safe
+        private val bootstrapClient by lazy {
+            OkHttpClient.Builder()
+                .protocols(listOf(Protocol.HTTP_2, Protocol.HTTP_1_1))
+                .build()
+        }
+
+        // https://de.wikipedia.org/wiki/DNS_over_HTTPS
+        // https://github.com/DigitaleGesellschaft/DNS-Resolver
+        val digitalSocietySwitzerlandDoH by lazy {
+            createDnsOverHttpsResolver(
+                url = "https://dns.digitale-gesellschaft.ch/dns-query",
+                ips = listOf("2a05:fc84::42", "2a05:fc84::43", "185.95.218.42", "185.95.218.43")
+            )
+        }
+
+        // https://www.quad9.net/news/blog/doh-with-quad9-dns-servers/
+        val quad9DoH by lazy {
+            createDnsOverHttpsResolver(
+                url = "https://dns.quad9.net/dns-query",
+                ips = listOf("2620:fe::fe", "2620:fe::fe:9", "9.9.9.9", "149.112.112.112"),
+            )
+        }
+
+        // https://developers.cloudflare.com/1.1.1.1/encryption/dns-over-https/make-api-requests/
+        val cloudflareDoH by lazy {
+            createDnsOverHttpsResolver(
+                url = "https://cloudflare-dns.com/dns-query",
+                ips = listOf("1.1.1.1"),
+            )
+        }
+
+        // https://developers.google.com/speed/public-dns/docs/doh
+        // https://developers.google.com/speed/public-dns/docs/using#google_public_dns_ip_addresses
+        val googleDoH by lazy {
+            createDnsOverHttpsResolver(
+                url = "https://dns.google/dns-query",
+                ips = listOf("8.8.8.8", "8.8.4.4", "2001:4860:4860::8888", "2001:4860:4860::8844")
+            )
+        }
+
+        private fun createDnsOverHttpsResolver(url: String, ips: List<String>): DnsOverHttps {
+            return DnsOverHttps.Builder()
+                .client(bootstrapClient)
+                .url(url.toHttpUrl())
+                .bootstrapDnsHosts(ips.map { InetAddress.getByName(it) })
+                .build()
+        }
+
+        val fakeDnsResolver by lazy {
+            object : Dns {
+                override fun lookup(hostname: String): List<InetAddress> {
+                    return listOf(InetAddress.getByName("127.0.0.1"))
+                }
+            }
+        }
     }
 }
 
--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/settings/NetworkSettingsHelper.kt
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/settings/NetworkSettingsHelper.kt
@@ -3,22 +3,10 @@ package de.marmaro.krt.ffupdater.settings
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
-import de.marmaro.krt.ffupdater.network.ProxyAuthenticator
-import okhttp3.Authenticator
-import okhttp3.Dns
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import okhttp3.OkHttpClient
-import okhttp3.Protocol
-import okhttp3.dnsoverhttps.DnsOverHttps
 import java.net.InetAddress
-import java.net.InetSocketAddress
 import java.net.Proxy
-import java.net.Proxy.Type.HTTP
-import java.net.Proxy.Type.SOCKS
+import java.net.Proxy.Type
 import java.net.UnknownHostException
-import java.security.KeyStore
-import java.security.SecureRandom
-import javax.net.ssl.*
 
 
 class NetworkSettingsHelper {
@@ -36,82 +24,53 @@ class NetworkSettingsHelper {
     val areUserCAsTrusted
         get() = preferences.getBoolean("network__trust_user_cas", false)
 
-    fun createSslSocketFactory(): Pair<SSLSocketFactory, X509TrustManager>? {
-        if (preferences.getBoolean("network__trust_user_cas", false)) {
-            return null // use the system trust manager
-        }
-
-        // https://developer.android.com/reference/java/security/KeyStore
-        val systemAndUserCAStore = KeyStore.getInstance("AndroidCAStore")
-        systemAndUserCAStore.load(null)
-
-        // https://stackoverflow.com/a/24401795
-        // https://github.com/bitfireAT/davx5-ose/blob/0e93a47d6d7277d3a18e31c6528f578c467a56ea/app/src/main/java/at/bitfire/davdroid/HttpClient.kt
-        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
-        trustManagerFactory.init(systemAndUserCAStore)
-
-        val keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm())
-        keyManagerFactory.init(systemAndUserCAStore, "keystore_pass".toCharArray())
-
-        val sslContext = SSLContext.getInstance("TLS")
-        sslContext.init(keyManagerFactory.keyManagers, trustManagerFactory.trustManagers, SecureRandom())
-
-        val trustManager = trustManagerFactory.trustManagers.first() as X509TrustManager
-        return Pair(sslContext.socketFactory, trustManager)
+    enum class DnsProvider {
+        // order of enums is important and must not change
+        SYSTEM, DIGITAL_SOCIETY_SWITZERLAND_DOH, QUAD9_DOH, CLOUDFLARE_DOH, GOOGLE_DOH, CUSTOM_SERVER, NO
     }
 
-    fun createDnsConfiguration(): Dns? {
-        // https://de.wikipedia.org/wiki/DNS_over_HTTPS
-        // https://github.com/DigitaleGesellschaft/DNS-Resolver
-        return when (preferences.getString("network__dns_provider", "0")) {
-            "0" -> null // use system settings, don't change the DNS setting
-            "1" -> digitalSocietySwitzerlandDoH
-            "2" -> quad9DoH
-            "3" -> cloudflareDoH
-            "4" -> googleDoH
-            "5" -> createDnsConfigurationFromUserInput(preferences)
-            "6" -> fakeDnsResolver
-            else -> throw IllegalArgumentException("invalid value for network__dns_provider")
-        }
-    }
+    val dnsProvider
+        get() = DnsProvider.values().getOrNull(
+            preferences.getString("network__dns_provider", "0")?.toIntOrNull() ?: 0
+        ) ?: DnsProvider.SYSTEM
 
-    private fun createDnsConfigurationFromUserInput(preferences: SharedPreferences): Dns? {
-        val customServer = preferences.getString("network__custom_doh_server", "")?.trim()
-        if (customServer == null || customServer.isEmpty()) {
-            preferences.edit()
-                .putString("network__dns_provider", "0")
-                .apply()
-            return null
-        }
-        // reset value to notify the user that the server connection details are invalid
-        if (!customServer.contains(",")) {
-            preferences.edit()
-                .putString("network__custom_doh_server", "")
-                .apply()
-            return null
-        }
+    data class DohConnectionDetails(
+        val host: String,
+        val ips: List<String>,
+    )
 
-        val arguments = customServer.split(",")
+    fun customDohServer(): DohConnectionDetails {
+        val rawString = preferences.getString("network__custom_doh_server", "")?.trim()
+            ?: throw IllegalArgumentException("Missing connection details for custom DoH server.")
+        if (!rawString.contains(",")) {
+            throw IllegalArgumentException("Wrong formatted connection details for the DoH server.")
+        }
+        val arguments = rawString.split(",")
         val server = arguments[0]
         val ips = arguments.subList(1, arguments.size)
-        return createDnsOverHttpsResolver(server, ips)
+        return DohConnectionDetails(server, ips)
     }
 
-    fun createProxyConfiguration(): Proxy? {
-        val proxyString = preferences.getString("network__proxy", "")?.trim()
-        if (proxyString == null || proxyString.isEmpty()) {
-            return null
-        }
 
-        // reset value to notify the user that the server connection details are invalid
-        if (proxyString.count { it == ':' } !in listOf(2, 4)) {
+    data class ProxyConnectionDetails(
+        val type: Type,
+        val host: InetAddress,
+        val port: Int,
+        val username: String?,
+        val password: String?
+    )
+
+    fun proxy(): ProxyConnectionDetails? {
+        val proxy = preferences.getString("network__proxy", "")?.trim() ?: return null
+        val proxyArguments = proxy.split(':')
+        if (proxyArguments.size !in listOf(3, 5)) {
             throw IllegalArgumentException("Invalid proxy configuration. Please fix the 'Proxy' setting.")
         }
-        val (typeString, ipString, portString) = proxyString.split(':')
 
+        val (typeString, ipString, portString) = proxyArguments
         val type = when (typeString) {
-            "SOCKS" -> SOCKS
-            "HTTP" -> HTTP
+            "SOCKS" -> Proxy.Type.SOCKS
+            "HTTP" -> Proxy.Type.HTTP
             else -> throw IllegalArgumentException("Invalid proxy configuration. Only SOCKS or HTTP are allowed. Please fix the 'Proxy' setting.")
         }
         val host = try {
@@ -122,82 +81,9 @@ class NetworkSettingsHelper {
         val port = portString.toIntOrNull()
             ?: throw IllegalArgumentException("Invalid proxy configuration. Please fix the 'Proxy' setting.")
 
-        return Proxy(type, InetSocketAddress(host, port))
-    }
+        val username = if (proxyArguments.size == 5) proxyArguments[3] else null
+        val password = if (proxyArguments.size == 5) proxyArguments[4] else null
 
-    fun createProxyAuthenticatorConfiguration(): Authenticator? {
-        val proxyString = preferences.getString("network__proxy", "")?.trim()
-        if (proxyString == null || proxyString.isEmpty()) {
-            return null
-        }
-
-        // reset value to notify the user that the server connection details are invalid
-        val (_, _, _, username, password) = when (proxyString.count { it == ':' }) {
-            2 -> return null // no username/password is given
-            3 -> throw IllegalArgumentException("Invalid proxy configuration. You have to specify a password.")
-            4 -> proxyString.split(':')
-            else -> throw IllegalArgumentException("Invalid proxy configuration format.")
-        }
-
-        return ProxyAuthenticator(username, password)
-    }
-
-    companion object {
-        // https://github.com/square/okhttp/blob/7768de7baaa992adcd384871cb8720873f6b8fd0/okhttp-dnsoverhttps/src/test/java/okhttp3/dnsoverhttps/DnsOverHttpsTest.java
-        // sharing an OkHttpClient is safe
-        private val bootstrapClient by lazy {
-            OkHttpClient.Builder()
-                .protocols(listOf(Protocol.HTTP_2, Protocol.HTTP_1_1))
-                .build()
-        }
-
-        // https://github.com/DigitaleGesellschaft/DNS-Resolver
-        val digitalSocietySwitzerlandDoH by lazy {
-            createDnsOverHttpsResolver(
-                url = "https://dns.digitale-gesellschaft.ch/dns-query",
-                ips = listOf("2a05:fc84::42", "2a05:fc84::43", "185.95.218.42", "185.95.218.43")
-            )
-        }
-
-        // https://www.quad9.net/news/blog/doh-with-quad9-dns-servers/
-        val quad9DoH by lazy {
-            createDnsOverHttpsResolver(
-                url = "https://dns.quad9.net/dns-query",
-                ips = listOf("2620:fe::fe", "2620:fe::fe:9", "9.9.9.9", "149.112.112.112"),
-            )
-        }
-
-        // https://developers.cloudflare.com/1.1.1.1/encryption/dns-over-https/make-api-requests/
-        val cloudflareDoH by lazy {
-            createDnsOverHttpsResolver(
-                url = "https://cloudflare-dns.com/dns-query",
-                ips = listOf("1.1.1.1"),
-            )
-        }
-
-        // https://developers.google.com/speed/public-dns/docs/doh
-        // https://developers.google.com/speed/public-dns/docs/using#google_public_dns_ip_addresses
-        val googleDoH by lazy {
-            createDnsOverHttpsResolver(
-                url = "https://dns.google/dns-query",
-                ips = listOf("8.8.8.8", "8.8.4.4", "2001:4860:4860::8888", "2001:4860:4860::8844")
-            )
-        }
-
-        private fun createDnsOverHttpsResolver(url: String, ips: List<String>): DnsOverHttps {
-            return DnsOverHttps.Builder()
-                .client(bootstrapClient)
-                .url(url.toHttpUrl())
-                .bootstrapDnsHosts(ips.map { InetAddress.getByName(it) })
-                .build()
-        }
-
-        val fakeDnsResolver by lazy {
-            object : Dns {
-                override fun lookup(hostname: String): List<InetAddress> {
-                    return listOf(InetAddress.getByName("127.0.0.1"))
-                }
-            }
-        }
+        return ProxyConnectionDetails(type, host, port, username, password)
     }
 }
