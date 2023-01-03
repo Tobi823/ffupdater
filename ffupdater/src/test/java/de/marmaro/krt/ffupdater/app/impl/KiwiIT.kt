4dae08be816f0d6a4a77bc95be5203736b8cd6d2
Tobi823 <ffupdater@mailbox.org>
2023-01-03 16:31:46 +0100

    - increase max allowed age of Kiwi

--- ffupdater/src/test/java/de/marmaro/krt/ffupdater/app/impl/KiwiIT.kt
+++ ffupdater/src/test/java/de/marmaro/krt/ffupdater/app/impl/KiwiIT.kt
@@ -20,7 +20,7 @@ internal class KiwiIT : BaseAppIT() {
         verifyThatDownloadLinkAvailable(result.downloadUrl)
         val releaseDate = ZonedDateTime.parse(result.publishDate, DateTimeFormatter.ISO_ZONED_DATE_TIME)
         val age = Duration.between(releaseDate, ZonedDateTime.now())
-        assertTrue(age.toDays() < 9 * 7) { "${age.toDays()} days is too old" }
+        assertTrue(age.toDays() < 12 * 7) { "${age.toDays()} days is too old" }
         assertTrue(result.firstReleaseHasAssets)
     }
 }
