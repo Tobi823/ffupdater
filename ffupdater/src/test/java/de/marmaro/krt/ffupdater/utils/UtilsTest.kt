5f07d99f3785acee794d29d36dfada3c3a402fa6
Tobi823 <ffupdater@mailbox.org>
2023-02-05 22:22:18 +0100

    - cleanup code

--- ffupdater/src/test/java/de/marmaro/krt/ffupdater/utils/UtilsTest.kt
+++ ffupdater/src/test/java/de/marmaro/krt/ffupdater/utils/UtilsTest.kt
@@ -3,17 +3,11 @@ package de.marmaro.krt.ffupdater.utils
 import android.os.Build
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.util.zip.ZipFile
 
 class UtilsTest {
 
     @Test
     fun getVersionAndCodenameForApiLevel_withAndroidR_returnCorrectText() {
         assertEquals("11 (R)", AndroidVersionCodes.getVersionForApiLevel(Build.VERSION_CODES.R))
-    }
-
-    @Test
-    fun test1() {
-        val test = ZipFile("/home/hacker/Downloads/F-Droid.apk")
     }
 }
