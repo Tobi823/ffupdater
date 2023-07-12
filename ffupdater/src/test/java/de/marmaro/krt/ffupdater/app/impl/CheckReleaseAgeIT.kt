b894b3df5f813a1e1b157a3796590a5584451414
Tobi823 <ffupdater@mailbox.org>
2023-07-12 14:00:12 +0200

    - increase max allowed age of Orbot

--- ffupdater/src/test/java/de/marmaro/krt/ffupdater/app/impl/CheckReleaseAgeIT.kt
+++ ffupdater/src/test/java/de/marmaro/krt/ffupdater/app/impl/CheckReleaseAgeIT.kt
@@ -53,7 +53,7 @@ class CheckReleaseAgeIT {
 //            TestData(Lockwise(GithubConsumer.INSTANCE), 84),
             TestData(Mulch, 56),
             TestData(MullFromRepo, 56),
-            TestData(Orbot, 84),
+            TestData(Orbot, 4 * 30),
             TestData(PrivacyBrowser, 56),
             TestData(TorBrowserAlpha, 56),
             TestData(TorBrowser, 56),
