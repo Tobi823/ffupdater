06d9f2120fed67fd74263458b7ea4442f9fa1d4d
Tobi823 <ffupdater@mailbox.org>
2023-10-05 22:45:17 +0200

    - fix bug from testing

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/app/impl/FFUpdater.kt
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/app/impl/FFUpdater.kt
@@ -27,7 +27,7 @@ object FFUpdater : AppBase() {
     override val supportedAbis = ALL_ABIS
 
     @Suppress("SpellCheckingInspection")
-    override val signatureHash = "f4e642bb85cbbcfd7302b2cbcbd346993a41067c27d995df492c9d0d38747e63"
+    override val signatureHash = "f4e642bb85cbbcfd7302b2cbcbd346993a41067c27d995df492c9d0d38747e62"
     override val installableByUser = false
     override val projectPage = "https://github.com/Tobi823/ffupdater"
     override val displayCategory = listOf(OTHER)
