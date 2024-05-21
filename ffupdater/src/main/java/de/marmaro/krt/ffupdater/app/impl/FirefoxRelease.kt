28d77bd82a40e47ce333e10e17c9686e7008543d
Tobi823 <ffupdater@mailbox.org>
2024-05-21 20:20:16 +0200

    Release version 79.2.0 (170) - Thanks for the Weblate contributions from: Scrambled777, Bahaa Zidan, minh, Oğuz Ersen, gallegonovato, winver, Raymond Nee, v1s7, Giovanni Donisi, Priit Jõerüüt, 109247019824, Kazushi Hayama, atilluF and MkQtS - fix download url for Firefox Nightly (Tobi823/ffupdater#579, thanks monstorix) - fix bug: Download correct app if the second update notification is clicked. (Tobi823/ffupdater#565) - fix bug: If you click on a update notification, FFUpdater will update the correct app and not the app from the first notification - try to fix update loop (Tobi823/ffupdater#576) - fix not working automatic APK deletion (Tobi823/ffupdater#569) - add button to retry a failed installation - inform users that Kiwi Browser is not truly open source (Tobi823/ffupdater#554) - fix tests, fix bugs, refactor code

--- ffupdater/src/main/java/de/marmaro/krt/ffupdater/app/impl/FirefoxRelease.kt
+++ ffupdater/src/main/java/de/marmaro/krt/ffupdater/app/impl/FirefoxRelease.kt
@@ -12,9 +12,10 @@ import de.marmaro.krt.ffupdater.device.ABI
 import de.marmaro.krt.ffupdater.device.DeviceAbiExtractor
 import de.marmaro.krt.ffupdater.network.exceptions.NetworkException
 import de.marmaro.krt.ffupdater.network.file.CacheBehaviour
-import de.marmaro.krt.ffupdater.network.github.GithubConsumer
-import de.marmaro.krt.ffupdater.network.github.GithubConsumer.GithubRepo
+import de.marmaro.krt.ffupdater.network.website.MozillaArchiveConsumer
 import de.marmaro.krt.ffupdater.settings.DeviceSettingsHelper
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 /**
  * https://github.com/mozilla-mobile/focus-android
@@ -35,41 +36,42 @@ object FirefoxRelease : AppBase() {
 
     @Suppress("SpellCheckingInspection")
     override val signatureHash = "a78b62a5165b4494b2fead9e76a280d22d937fee6251aece599446b2ea319b04"
-    override val projectPage = "https://github.com/mozilla-mobile/firefox-android"
+    override val projectPage = "https://firefox-ci-tc.services.mozilla.com/tasks/index/mobile.v3.firefox-android.apks.fenix-release.latest/arm64-v8a"
     override val displayCategory = listOf(FROM_MOZILLA)
-    val REPOSITORY = GithubRepo("mozilla-mobile", "firefox-android", 13)
+
 
     @MainThread
     @Throws(NetworkException::class)
     override suspend fun fetchLatestUpdate(context: Context, cacheBehaviour: CacheBehaviour): LatestVersion {
-        val fileSuffix = findFileSuffix()
-        val result = GithubConsumer.findLatestRelease(
-            repository = REPOSITORY,
-            isValidRelease = { !it.isPreRelease && """^Firefox \d""".toRegex().containsMatchIn(it.name) },
-            isSuitableAsset = { it.nameStartsAndEndsWith("fenix-", "-$fileSuffix") },
-            cacheBehaviour = cacheBehaviour,
-            requireReleaseDescription = false,
-        )
-        val version = result.tagName
-            .removePrefix("fenix-v") //convert fenix-v111.1.0 to 111.1.0
-            .removePrefix("v") //fallback if the tag naming schema changed
+        val version = findLatestVersion(cacheBehaviour)
+        val abi = findAbiString()
+        val page = "https://archive.mozilla.org/pub/fenix/releases/$version/android/fenix-$version-android-$abi/"
+        val downloadUrl = "${page}fenix-$version.multi.android-$abi.apk"
+        val dateTime = MozillaArchiveConsumer.findDateTimeFromPage(page, cacheBehaviour)
         return LatestVersion(
-            downloadUrl = result.url,
+            downloadUrl = downloadUrl,
             version = version,
-            publishDate = result.releaseDate,
-            exactFileSizeBytesOfDownload = result.fileSizeBytes,
+            publishDate = dateTime.toString(),
+            exactFileSizeBytesOfDownload = null,
             fileHash = null,
         )
     }
 
-    private fun findFileSuffix(): String {
-        val fileSuffix = when (DeviceAbiExtractor.findBestAbi(supportedAbis, DeviceSettingsHelper.prefer32BitApks)) {
-            ABI.ARMEABI_V7A -> "armeabi-v7a.apk"
-            ABI.ARM64_V8A -> "arm64-v8a.apk"
-            ABI.X86 -> "x86.apk"
-            ABI.X86_64 -> "x86_64.apk"
+    private suspend fun findLatestVersion(cacheBehaviour: CacheBehaviour): String {
+        val url = "https://archive.mozilla.org/pub/fenix/releases/"
+        val versionRegex = Regex("""(\d+)\.(\d+)\.?(\d+)?""")
+        val version = MozillaArchiveConsumer.findLatestVersion(url, versionRegex, cacheBehaviour)
+        return version
+    }
+
+    private fun findAbiString(): String {
+        val abiString = when (DeviceAbiExtractor.findBestAbi(supportedAbis, DeviceSettingsHelper.prefer32BitApks)) {
+            ABI.ARMEABI_V7A -> "armeabi-v7a"
+            ABI.ARM64_V8A -> "arm64-v8a"
+            ABI.X86 -> "x86"
+            ABI.X86_64 -> "x86_64"
             else -> throw IllegalArgumentException("ABI is not supported")
         }
-        return fileSuffix
+        return abiString
     }
 }
